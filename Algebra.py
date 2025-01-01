caa0a8eb32a806ada61bb8ff0cb1fd260993c236
Grayson Bell <graysonjarodbell@gmail.com>
2025-01-01 22:54:01 +1000

    Basics

---
+++ Algebra.py
@@ -0,0 +1,16 @@
+x = 1
+y = 2
+z = x * y
+print(z)
+
+addition = x + y
+print(addition)
+
+subtraction = x - y
+print(subtraction)
+
+Modulus = x % y
+print(Modulus)
+
+Exponent = x ** y
+print(Exponent)
---
+++ Calculus.py
@@ -0,0 +1,12 @@
+#Intagrals
+
+
+
+
+
+
+
+
+
+
+#Derivatives
